1bcf20e17872dbf56b5ae78c24c7c73146b803ef
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-11-04 14:42:52 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// css
+import "./styles/index.css";
+
+function App() {
+  return (
+    <main>
+      <div className="flex justify-center items-center min-h-screen">
+        <h1 className="text-green-700 hover:text-2xl cursor-pointer">
+          Hello World!
+        </h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
