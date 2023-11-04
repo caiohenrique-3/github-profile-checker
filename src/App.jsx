c7b21927253941b755129f3891a49202b2db150b
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-11-04 18:55:30 -0300

    feat: Add error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // dependencies
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 // css
 import "./styles/index.css";
@@ -9,6 +9,7 @@ function App() {
   const [showErrorMessage, setShowErrorMessage] = useState(false);
   const [loadProfile, setLoadProfile] = useState(false);
   const [profileContent, setProfileContent] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -32,21 +33,40 @@ function App() {
     }
   };
 
-  function handleSearchClick() {
+  function handleSearchClick(event) {
     event.preventDefault();
 
     if (!showErrorMessage) {
       setLoadProfile(true);
 
       fetch(`https://api.github.com/users/${inputValue}`)
-        .then((response) => response.json())
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
         .then((data) => {
+          if (data.message === "Not Found") {
+            throw new Error("User not found");
+          }
           setProfileContent(data);
         })
         .catch((error) => {
           console.error("Error:", error);
+          setLoadProfile(false);
+          setShowErrorMessage(true);
+          setShowPopup(true);
+          resetApp();
         });
     }
+  }
+
+  function resetApp() {
+    setInputValue("");
+    setShowErrorMessage(false);
+    setLoadProfile(false);
+    setShowPopup(true);
   }
 
   return (
@@ -108,6 +128,56 @@ function App() {
           >
             GitHub profile link
           </a>
+        </div>
+      )}
+
+      {showPopup && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div
+              className="fixed inset-0 transition-opacity"
+              aria-hidden="true"
+            >
+              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3
+                      className="text-lg leading-6 font-medium text-gray-900"
+                      id="modal-title"
+                    >
+                      User Not Found
+                    </h3>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        The username you entered does not exist on GitHub.
+                        Please try again with a different username.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  onClick={() => setShowPopup(false)}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </main>
